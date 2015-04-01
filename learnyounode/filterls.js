a8a1bcde4fd3de312eefe514c1b2454b1a18b7d4
geoffdavis92 <geoffdavis92@gmail.com>
2015-04-01 14:34:42 -0500

    completed

--- learnyounode/filterls.js
+++ learnyounode/filterls.js
@@ -22,3 +22,16 @@ var dir = fs.readdir(file,function callback(err,list){
 	}
 })
 
+/* OFFICIAL SOLUTION ::
+
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
+})
+
+*/
